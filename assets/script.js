6ed9118a6b698870898f11bb57ba0d6f2f953732
Guled <guled@ugbads-MacBook-Pro.local>
2023-12-19 23:29:36 +0000

    refactored to use jquery

--- assets/script.js
+++ assets/script.js
@@ -4,27 +4,29 @@ $("#currentDay").text(today.format("D MMM YYYY"));
 
 console.log(today)
 
-function saveText(textAreaId) {
-    // Get the text from the specified text area
-    var text = document.getElementById(textAreaId).value;
+
+  // Function to save text to local storage using jQuery
+  function saveText(textAreaId) {
+    // Get the text from the specified text area using jQuery
+    var text = $('#' + textAreaId).val();
 
     // Use localStorage to save the text
     localStorage.setItem(textAreaId, text);
   }
 
-  // Function to load and display text from local storage
+  // Function to load and display text from local storage using jQuery
   function loadText() {
     // Loop through each text area
     for (var i = 1; i <= 24; i++) {
       // Create the localStorage key for the current text area
       var key = 'timeBlock' + i;
 
-      // Get the text from localStorage
+      // Get the text from localStorage using jQuery
       var savedText = localStorage.getItem(key);
 
       // If there is saved text, display it in the corresponding text area
       if (savedText !== null) {
-        document.getElementById(key).value = savedText;
+        $('#' + key).val(savedText);
       }
     }
   }
